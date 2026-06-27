88c66d0af6b0f63cbd4da812f4b7c4d1610a0e0b
abdullah_al_habal <dbnkalhbalb@gmail.com>
2026-06-27 17:33:51 +0300

    Update composer.json with packages and Modules namespace

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,7 +1,6 @@
 <?php
 declare(strict_types=1);
 
-use Illuminate\Contracts\Config\Repository as ConfigRepository;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -12,16 +11,6 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         health: '/up',
     )
-    ->withConfig(function (ConfigRepository $config): void {
-        $config->set([], true);
-
-        $path = __DIR__ . '/../modules/Core/Config';
-
-        foreach (glob($path . '/*.php') as $file) {
-            $key = basename($file, '.php');
-            $config->set($key, require $file);
-        }
-    })
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->prepend(AssignRequestUuid::class);
     })
--- modules/Core/Providers/ApplicationServiceProvider.php
+++ modules/Core/Providers/ApplicationServiceProvider.php
@@ -3,15 +3,30 @@ declare(strict_types=1);
 
 namespace Modules\Core\Providers;
 
+use Illuminate\Contracts\Config\Repository as ConfigRepository;
 use Illuminate\Support\ServiceProvider;
 
 final class ApplicationServiceProvider extends ServiceProvider
 {
-    public function register(): void {}
+    public function register(): void
+    {
+        $this->loadConfig();
+    }
 
     public function boot(): void
     {
         $this->loadMigrationsFrom(__DIR__ . '/../Database/Migrations');
         $this->loadTranslationsFrom(__DIR__ . '/../lang', 'core');
+    }
+
+    private function loadConfig(): void
+    {
+        $config = $this->app->make(ConfigRepository::class);
+        $path = __DIR__ . '/../Config';
+
+        foreach (glob($path . '/*.php') as $file) {
+            $key = basename($file, '.php');
+            $config->set($key, require $file);
+        }
     }
 }
