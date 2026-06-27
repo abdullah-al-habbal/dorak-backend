187cee67d73a46c8a28807150ceb887e26685b11
abdullah_al_habal <dbnkalhbalb@gmail.com>
2026-06-27 17:26:03 +0300

    Create Core module directory structure

---
+++ modules/Core/Database/Factories/UserFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Modules\Core\Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\Hash;
+use Modules\Core\Models\UserModel;
+
+class UserFactory extends Factory
+{
+    protected $model = UserModel::class;
+
+    public function definition(): array
+    {
+        return [
+            'name'              => fake()->name(),
+            'email'             => fake()->unique()->safeEmail(),
+            'email_verified_at' => now(),
+            'password'          => Hash::make('password'),
+            'remember_token'    => \Illuminate\Support\Str::random(10),
+        ];
+    }
+}
---
+++ modules/Core/Models/UserModel.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Modules\Core\Models;
+
+use Illuminate\Database\Eloquent\Attributes\Fillable;
+use Illuminate\Database\Eloquent\Attributes\Hidden;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Modules\Core\Database\Factories\UserFactory;
+
+#[Fillable(['name', 'email', 'password'])]
+#[Hidden(['password', 'remember_token'])]
+class UserModel extends Authenticatable
+{
+    use HasFactory, Notifiable;
+
+    protected static function newFactory(): UserFactory
+    {
+        return UserFactory::new();
+    }
+
+    protected function casts(): array
+    {
+        return [
+            'email_verified_at' => 'datetime',
+            'password' => 'hashed',
+        ];
+    }
+}
